4bb27c63097288318c213a004b014bea1e51c8a2
Doi90 <david.wilkinson.research@gmail.com>
2025-10-15 15:29:29 +1100

    Document SIPS_comments

---
+++ R/SIPS_comments.R
@@ -0,0 +1,23 @@
+#' SIPS - Known Outcomes: Comments
+#'
+#' The free-text comments of the participants on the justifications of the other participants. 
+#' This data is stored in long format with one row per participant, claim, elicitation round, 
+#' and question.
+#'
+#' @format A tibble with 2 rows and 11 column variables:
+#' \describe{
+#' \item{round}{IDEA Protocol elicitation round. Either "round_1" or "round_2".}
+#' \item{paper_id}{Unique, alphanumeric identifier for the paper/claim being assessed.}
+#' \item{user_name}{Unique, anonymised, alphanumeric identifier for the participant.}
+#' \item{question}{Question type.}
+#' \item{justification_id}{Unique, alphanumeric identifier for the justification.}
+#' \item{comment_id}{Unique, alphanumeric identifier for the comment.}
+#' \item{commenter}{Unique, anonymised, alphanumeric identifier for the participant who authored the comment.}
+#' \item{comment}{Participants' free-text responses commenting on a justification.}
+#' \item{vote_count}{Count of the number of votes by other participants on this justification.}
+#' \item{vote_sum}{Sum of the votes (-1,1) by other participants on this justification.}
+#' \item{group}{Elicitation group the participant belonged to.}
+#' }
+#' @examples
+#' SIPS_comments
+"SIPS_comments"
