ad645c9e0eb9eb02ccf0c3d88f59cda69a5096ad
Doi90 <david.wilkinson.research@gmail.com>
2025-10-15 15:09:27 +1100

    Document p2_ratings

---
+++ R/p2_ratings.R
@@ -0,0 +1,17 @@
+#' Phase Two - Single Trace: Ratings
+#'
+#' The elicited numeric ratings of the participants. This data is stored in long format with one 
+#' row per participant, claim, elicitation round, and question.
+#'
+#' @format A tibble with 46,007 rows and 6 column variables:
+#' \describe{
+#' \item{round}{IDEA Protocol elicitation round. Either "round_1" or "round_2".}
+#' \item{paper_id}{Unique, alphanumeric identifier for the paper/claim being assessed.}
+#' \item{user_name}{Unique, anonymised, alphanumeric identifier for the participant.}
+#' \item{question}{Question type.}
+#' \item{element}{Component of response to question. Some questions use three-point elicitation.}
+#' \item{value}{Participant's response to question. Ratings are reported on percentage scale (0-100), binary questions use -1/1 notation.}
+#' }
+#' @examples
+#' p2_ratings
+"p2_ratings"
