29d390e7b4ee200c178b1521d8bb17f7a78934e6
Doi90 <david.wilkinson.research@gmail.com>
2025-10-15 15:09:45 +1100

    Document SIPS ratings

---
+++ R/SIPS_ratings.R
@@ -0,0 +1,18 @@
+#' SIPS - Known Outcomes: Ratings
+#'
+#' The elicited numeric ratings of the participants. This data is stored in long format with one 
+#' row per participant, claim, elicitation round, and question.
+#'
+#' @format A tibble with 6,880 rows and 7 column variables:
+#' \describe{
+#' \item{round}{IDEA Protocol elicitation round. Either "round_1" or "round_2".}
+#' \item{paper_id}{Unique, alphanumeric identifier for the paper/claim being assessed.}
+#' \item{user_name}{Unique, anonymised, alphanumeric identifier for the participant.}
+#' \item{question}{Question type.}
+#' \item{element}{Component of response to question. Some questions use three-point elicitation.}
+#' \item{value}{Participant's response to question. Ratings are reported on percentage scale (0-100), binary questions use -1/1 notation.}
+#' \item{group}{Elicitation group the participant belonged to.}
+#' }
+#' @examples
+#' SIPS_ratings
+"SIPS_ratings"
